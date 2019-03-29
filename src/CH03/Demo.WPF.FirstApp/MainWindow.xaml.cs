cf8c602613897e23f8db1db9f7a26cf9d0e69f1e
Kunal Chowdhury <kunal-chowdhury@users.noreply.github.com>
2019-03-29 21:18:15 +0530

    Chapter 03 Source Code (#1)

---
+++ src/CH03/Demo.WPF.FirstApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Demo.WPF.FirstApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello WPF Message Box");
+        }
+    }
+}
