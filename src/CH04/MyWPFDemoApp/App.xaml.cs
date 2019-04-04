badd8ebfb05b78b0e69392a364aa676904d8fb10
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2019-04-04 20:19:49 +0530

    Chapter 04 Source Code (#2)

---
+++ src/CH04/MyWPFDemoApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyWPFDemoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MobileServiceClient MobileService = 
+            new MobileServiceClient("https://kunal-chowdhury-service.azurewebsites.net");
+    }
+}
---
+++ src/CH04/MyWPFDemoApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWPFDemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OnRefreshButtonClicked(this, new RoutedEventArgs());
+        }
+
+        private async void OnSaveButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var taskDescription = txbTaskDescription.Text;
+
+            if (!string.IsNullOrWhiteSpace(taskDescription))
+            {
+                var isComplete = chkComplete.IsChecked == true;
+                var todoItem = new TodoItem { Text = taskDescription, Complete = isComplete };
+
+                txbTaskDescription.Text = string.Empty;
+                chkComplete.IsChecked = false;
+
+                await App.MobileService.GetTable<TodoItem>().InsertAsync(todoItem);
+                OnRefreshButtonClicked(sender, e);
+            }
+        }
+
+        private async void OnRefreshButtonClicked(object sender, RoutedEventArgs e)
+        {
+            lstDetails.ItemsSource = await App.MobileService.GetTable<TodoItem>().ReadAsync();
+        }
+
+        private async void OnDeleteButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if(lstDetails.SelectedItem is TodoItem item)
+            {
+                await App.MobileService.GetTable<TodoItem>().DeleteAsync(item);
+                OnRefreshButtonClicked(sender, e);
+            }
+        }
+    }
+}
