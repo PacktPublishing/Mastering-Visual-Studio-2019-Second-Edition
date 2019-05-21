1de29434cc2e235a1e04a1f61025eefef1c69bb8
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2019-05-21 22:10:30 +0530

    Chapter 09 Source Code

---
+++ src/CH09/TestClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestClassLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bdd8dc0-7084-4598-b65b-e39ea06e335b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/CH09/TestClassLibrary/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestClassLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTwoInstanceOfPersonInstancesAreNotEqual()
+        {
+            var person1 = new Person();
+            var person2 = new Person();
+
+            var status = person1 == person2;
+
+            Assert.IsFalse(status);
+        }
+
+        [TestMethod]
+        public void TestAssigningOneObjectToAnotherReferencesSameObject()
+        {
+            var person1 = new Person();
+            var person2 = person1;
+
+            var status = person1 == person2;
+
+            Assert.IsTrue(status);
+        }
+
+        [TestMethod]
+        public void TestCloningOneObjectReturnsDifferentInstance()
+        {
+            var person1 = new Person();
+            var person2 = (Person)person1.Clone();
+
+            var status = person1 == person2;
+
+            Assert.IsFalse(status);
+        }
+
+        [TestMethod]
+        public void TestCloningPopulatesThePropertiesProperly()
+        {
+            var person1 = new Person();
+            var person2 = (Person)person1.Clone();
+
+            var status = person1 == person2;
+
+            Assert.IsFalse(status);
+            Assert.AreEqual(person1.ID, person2.ID);
+            Assert.AreEqual(person1.Name, person2.Name);
+            Assert.AreEqual(person1.Address, person2.Address);
+        }
+    }
+}
