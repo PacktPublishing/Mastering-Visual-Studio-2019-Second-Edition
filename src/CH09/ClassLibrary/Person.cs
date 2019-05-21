4cf6eee1bdba6b7a5732a589a4df09d8c0f136c3
Kunal Chowdhury <kunal-chowdhury@users.noreply.github.com>
2019-05-21 22:12:14 +0530

    Chapter 09 Source Code (#5)

---
+++ src/CH09/ClassLibrary/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Person : ICloneable
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
